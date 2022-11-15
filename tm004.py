affdef9700dd6c8b812198761d9a6647be0ef401
51241 <3454969108@qq.com>
2022-11-15 21:54:16 +0800

    test

---
+++ tm004.py
@@ -0,0 +1,9 @@
+def tm004():
+    import time
+    import datetime
+##    data =input("请输入一个日期（例如2018—07—14）：")
+##    st = time.strptime(data,"%Y-%m-%d")
+##    num = st.tm_mday
+##    print(num)
+    today1 = datetime.date.today()
+    print(today1)
